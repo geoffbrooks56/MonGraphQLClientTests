5717e76491069406c211c4243f237f81105b481e
geoffbrooks56 <164505343+geoffbrooks56@users.noreply.github.com>
2024-07-12 11:17:37 -0400

    Add files via upload

---
+++ BaseFixture.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace MonGraphQLClientTests;
+public class BaseFixture : IDisposable
+{
+	public string BoardID;
+	public string APIKey;
+	public string APIUrl;
+	public HttpClient HttpClient { get; set; }
+
+	public BaseFixture()
+	{
+		var config = InitConfiguration();
+
+		BoardID = config.GetSection("Values:MondayMasterBoardId").Value;
+		APIKey = config.GetSection("Values:MondayAPIToken").Value;
+		APIUrl = config.GetSection("Values:MondayURL").Value;
+	}
+
+	public void Dispose()
+	{
+		GC.SuppressFinalize(this);
+	}
+
+	public static IConfiguration InitConfiguration()
+	{
+		var config = new ConfigurationBuilder()
+		    .AddJsonFile("local.settings.json")			
+			.Build();
+
+		return config;
+	}
+}
---
+++ MonBoardTests.cs
@@ -0,0 +1,33 @@
+﻿using Xunit;
+
+namespace MonGraphQLClientTests;
+
+[Collection("MonGraphQLClientTests collection")]
+public class MonBoardTests
+{
+	BaseFixture fixture;
+	public MonBoardTests(BaseFixture fixture)
+	{
+		this.fixture = fixture;
+	}
+
+	[Fact]
+	public async Task Test1()
+	{
+		await Task.Delay(100);
+
+		string boardid = fixture.BoardID;
+
+		string apiKey = fixture.APIKey;
+
+		string apiUrl = fixture.APIUrl;
+
+		Assert.False(string.IsNullOrEmpty(boardid));
+
+		Assert.False(string.IsNullOrEmpty(apiKey));
+
+		Assert.False(string.IsNullOrEmpty(apiUrl));
+	}
+
+}
+
